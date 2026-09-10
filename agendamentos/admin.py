0e880b035af989cbc2027a487b9349a2de393b9f
Dalessandro Santos <dalessandrosantos83@gmail.com>
2026-09-09 21:00:00 -0300

    Adiciona tabelas de [Nome1] e [Nome2] e registra no Django Admin

--- agendamentos/admin.py
+++ agendamentos/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Quadra, Reserva
 
-# Register your models here.
+@admin.register(Quadra)
+class QuadraAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'tipo')
+    search_fields = ('nome',)
+    list_filter = ('tipo',)
+
+@admin.register(Reserva)
+class ReservaAdmin(admin.ModelAdmin):
+    list_display = ('quadra', 'usuario', 'horario_inicio', 'horario_fim')
+    search_fields = ('qudra__nome', 'usuario__username')
--- agendamentos/models.py
+++ agendamentos/models.py
@@ -1,7 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Quadra(models.Model):
     nome = models.CharField(max_length=50)
     tipo = models.CharField(max_length=50)
-    preco_hora = models.DecimalField(max_digits=10, decimal_places=2)
+    preco_hora = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Preço hora')
 
+    class Meta:
+        verbose_name_plural = 'Quadras'
+
+    def __str__(self):
+        return self.nome
+
+
+class Reserva(models.Model):
+    quadra = models.ForeignKey(Quadra, on_delete=models.PROTECT)
+    usuario = models.ForeignKey(User, on_delete=models.PROTECT)
+    data = models.DateTimeField(auto_now_add=True)
+    horario_inicio = models.DateTimeField()
+    horario_fim = models.DateTimeField()
+
+    class Meta:
+        verbose_name_plural = 'Reservas'
+        ordering = ['-horario_inicio']
+
+    def __str__(self):
+        return f'{self.quadra} - {self.horario_inicio.strftime("%d/%m/%Y %H:%M")}'
